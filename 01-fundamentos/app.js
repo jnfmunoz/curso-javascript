66e39494f311ef97346fd3e4b9e6ad184b707347
jnfmunoz <juanmunozgrau@gmail.com>
2024-09-29 03:28:52 -0300

    commit-1

---
+++ 01-fundamentos/app.js
@@ -0,0 +1,15 @@
+
+// console.log(console.log('Hola Mundo'));
+
+// var b = 10;
+// const c = 10;
+
+// a = 20;
+// b = 30;
+
+let a = 10, b = 20;
+let c = 10, 
+    d= 10, 
+    x = a + b;
+
+console.log(x);
