45cd22cb01e3be72e6d8e9b70b52800944033172
jnfmunoz <juanmunozgrau@gmail.com>
2024-11-16 19:22:03 -0300

    commit-3

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,16 @@
+// Métodos del objeto window
+
+
+// alert('Hola Mundo');
+
+
+// let nombre = prompt('Cuál es tu nombre?', 'Sin nombre');
+// let nombre = prompt('Cuál es tu nombre?');
+// console.log(nombre);
+// console.log('****' + nombre + '****');
+
+// const seleccion = confirm('Está seguro de borrar esto¿?');
+// console.log(seleccion);
+
+// para node
+console.log(global);
---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,41 @@
+// alert('hola desde app.js');
+
+// console.log(console.log('Hola Mundo'));
+
+// var b = 10;
+// const c = 10;
+
+// a = 20;
+// b = 30;
+
+let a = 10, b = 20;
+// let c = 30, 
+//     d= 40;
+let x = a + b;
+
+// console.log(x);
+// console.warn(x);
+// console.error(x);
+
+// console.log('a', a);
+// console.log('b', b);
+
+// %c: Es un marcador de formato especial usado en console.log
+// console.log('%c Mis variables', 'color:blue; font-weight: bold');
+// console.log({ a });
+// console.log({ b });
+
+let c = 'Hola ', 
+    d= 'Spiderman';
+
+// console.table({ a,b,c,d,x });
+
+const saludo = c + d;
+// console.log(saludo);
+
+c = 'Hola de nuevo';
+
+let miNuevoNombre = 'Juan Muñoz';
+
+
+
