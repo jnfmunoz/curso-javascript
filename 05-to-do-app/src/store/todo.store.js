f122842a9186b5e1248c644f3fbf0bafa81673cc
jnfmunoz <juanmunozgrau@gmail.com>
2024-12-23 02:36:34 -0300

    commit-25

--- 05-to-do-app/src/store/todo.store.js
+++ 05-to-do-app/src/store/todo.store.js
@@ -1,6 +1,6 @@
-import {Todo} from '../todos/models/todo.model';
+import { Todo } from '../todos/models/todo.model';
 
-const Filter = {
+const Filters = {
     All: 'All',
     Completed: 'Completed',
     Pending: 'Pending',
@@ -13,7 +13,7 @@ const state = {
         new Todo('Piedra del tiempo'),
         
     ],
-    filter: Filter.All,
+    filter: Filters.All,
 
 }
 
@@ -22,7 +22,61 @@ const initStore = () => {
     console.log('Init Store 🦎');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+const getTodos = (filter = Filters.All) => {
+    switch(filter){
+        case Filters.All: 
+            return [...state.todos]; // operador spread para retornar un nuevo arreglo, a pesar de que los objetos los pasamos como referencia
+        case Filters.Completed:
+            return state.filter() 
+        
+    }
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId Todo identifier
+ */
+const toggleTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+const setFilter = (newFilter=Filters.All) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
 
 export default{
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
+
