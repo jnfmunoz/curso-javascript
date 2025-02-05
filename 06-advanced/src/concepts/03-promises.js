4c8392ec9dbf02bbf9978a2602e609edb0b843ea
jnfmunoz <juanmunozgrau@gmail.com>
2025-02-04 23:58:19 -0300

    commit-39

--- 06-advanced/src/concepts/03-promises.js
+++ 06-advanced/src/concepts/03-promises.js
@@ -12,8 +12,8 @@ export const promiseComponent = (element) => {
 
     const renderTwoHeros = (hero1, hero2) => {
         element.innerHTML = `
-            <h1>Error:</h1>
-            <h3>${error}</h3>
+            <h1>${ hero1.name }</h1>
+            <h1>${ hero2.name }</h1>
         `;
 
     };
@@ -29,9 +29,18 @@ export const promiseComponent = (element) => {
     const id1 = '5d86371f2343e37870b91ef1';
     const id2 = '5d86371fd55e2e2a30fe1ccb2';
     
+    // findHero(id1)
+    //     // .then( superHero => renderHero (superHero));
+    //     .then(renderHero)
+    //     .catch(error => renderError(error));
+
     findHero(id1)
-        // .then( superHero => renderHero (superHero));
-        .then(renderHero)
+        .then((hero1) =>{
+            findHero(id2)
+                .then(hero2 => {
+                    renderTwoHeros(hero1, hero2);
+                });
+        })
         .catch(error => renderError(error));
 
 }
