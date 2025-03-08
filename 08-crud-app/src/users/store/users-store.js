54ffb7be7cf45fa58be052522e51ae1dfb5a328d
jnfmunoz <juanmunozgrau@gmail.com>
2025-03-08 17:59:46 -0300

    commit-51

---
+++ 08-crud-app/src/users/store/users-store.js
@@ -0,0 +1,33 @@
+import { loadUsersByPage } from "../uses-cases/load-users-by-page";
+
+
+const state = {
+    currentPage: 0,
+    users: [],
+}
+
+const loadNextPage = async() => {
+    await loadUsersByPage(state.currentPage + 1);
+}
+
+const loadPreviousPage = async() => {
+    throw new Error('Not implemented');
+}
+
+const onUserChanged = () => {
+    throw new Error('Not implemented');
+}
+
+const reloadPage = async() => {
+    throw new Error('Not implemented');
+}
+
+export default {
+    loadNextPage,
+    loadPreviousPage,
+    onUserChanged,
+    reloadPage,
+
+    getUser: () => [ ...state.users],
+    getCurrentPage: () => state.currentPage,
+}
